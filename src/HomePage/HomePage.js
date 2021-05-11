3a509d439a20e0b35921cc9cb3ec1a20bfae0c2f
Md-Kais <mdkais3@gmail.com>
2021-05-11 16:53:57 +0600

    update 6.66

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -3,16 +3,15 @@ import SidebarManual from '../Components/SidebarManual/SideIndex';
 import NavbarManual from '../Components/NavbarManual/NavIndex';
 
 import HomeSection from '../Components/HomeSection/HomeSection';
-import AboutMe from '../Components/AboutPage/AboutMe/AboutMe';
-import Skill from '../Components/AboutPage/Skill/Skill';
+
 import About from '../Components/AboutPage/About/About';
-import Fade from 'react-reveal/Fade';
+
 
 
 
 import ProjectCard from '../Components/Project/ProjectCard/ProjectCard';
 import Project from '../Components/Project/Project/Project';
-import { Router, Route } from 'react-router';
+
 import Blog from '../Components/Blog/Blog';
 import Contact from '../Components/Contact/Contact';
 import Footer from '../Components/Footer/Footer';
@@ -26,7 +25,7 @@ const HomePage = () => {
     };
     return (
         <>
-            <Fade left cascade>
+            
                 <div>
                     <SidebarManual isOpen={isOpen} toggle={toggle}></SidebarManual>
                     <NavbarManual toggle={toggle}></NavbarManual>
@@ -43,8 +42,9 @@ const HomePage = () => {
                     <Contact></Contact>
                     <Resume></Resume>
                     <Footer></Footer>
+                    
                 </div>
-            </Fade>
+           
         </>
 
     )
